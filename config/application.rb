83d3b9339df2b31e4b1c4a6b7930a919a1169a5c
Liz Bayardelle <lizbayardelle@gmail.com>
2018-08-03 21:49:39 -0700

    Assets Precompiled

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,9 @@ module MAB
     # FOR DELAYED_JOB
     config.active_job.queue_adapter = :delayed_job
 
+    # serve assets
+    config.serve_static_assets = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
